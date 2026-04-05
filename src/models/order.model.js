fb3a9279378d3bc1b28a733168ccdf66eae153cd
Shrawan <shrwnkumar08@gmail.com>
2026-04-05 17:04:25 +0530

    order model created

---
+++ src/models/order.model.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    productName: String,
+    monthlyRent: Number,
+    deposit: Number,
+    quantity: {
+      type: Number,
+      default: 1,
+      min: 1,
+    },
+    tenure: {
+      type: Number,
+      required: true,
+      enum: [3, 6, 12],
+    },
+  },
+);
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [orderItemSchema],
+    totalRent: {
+      type: Number,
+      required: true,
+    },
+    totalDeposit: {
+      type: Number,
+      required: true,
+    },
+    grandTotal: {
+      type: Number,
+      required: true,
+    },
+    deliverDate: {
+      type: Date,
+      required: [true, "Delivery date is required"],
+    },
+    deliveryAddress: {
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: String,
+        required: true,
+      },
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "delivered", "returned", "cancelled"],
+      default: "pending",
+    },
+    rentalStartDate: Date,
+    rentalEndDate: Date,
+    notes: String,
+  },
+  { timestamps: true },
+);
+
+
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
