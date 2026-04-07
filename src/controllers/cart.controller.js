7f7d55722027987c1c8602b113955dbff0a69a01
Shrawan <shrwnkumar08@gmail.com>
2026-04-07 18:53:38 +0530

    decrease quantity of product and delete the cart  controller created

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -90,5 +90,34 @@ export const updateCartItem = async (req, res, next) => {
   }
 };
 
+export const removeFromCart = async (req, res, next) => {
+  try {
+    const cart = await Cart.findOne({ user: req.user._id });
+    if (!cart) {
+      return res.status(404).json({ success: false, message: 'Cart not found.' });
+    }
+ 
+    cart.items = cart.items.filter(
+      (item) => item.product.toString() !== req.params.productId
+    );
+ 
+    await cart.save();
+    await cart.populate('items.product');
+    res.json({ success: true, message: 'Item removed from cart.', cart });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const clearCart = async (req, res, next) => {
+  try {
+    await Cart.findOneAndUpdate({ user: req.user._id }, { items: [] });
+    res.json({ success: true, message: 'Cart cleared.' });
+  } catch (error) {
+    next(error);
+  }
+};
+
 
 
--- src/router/cart.route.js
+++ src/router/cart.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getCart, addToCart, updateCartItem} from "../controllers/cart.controller.js";
+import {getCart, addToCart, updateCartItem, removeFromCart, clearCart} from "../controllers/cart.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
 
@@ -16,9 +16,9 @@ router.post("/cart", protect, addToCart);
 
 router.put("/cart/:id", protect, updateCartItem);
 
-router.delete("/cart/:id", ()=>console.log("user can delete the product from the cart "));
+router.delete("/cart/:id", protect, removeFromCart);
 
-router.delete("/cart/clear", ()=>console.log("admin can delete the cart "));
+router.delete("/cart/clear", protect, clearCart);
 
 
 
